cd8856935b517250481f79c1acc24a19987f85bf
DanicaMassie <S11810002@student.unklab.ac.id>
2020-12-08 11:38:46 +0800

    8th commit(fix)

--- covidtracker/src/pages/Global.js
+++ covidtracker/src/pages/Global.js
@@ -1,12 +1,13 @@
 import React, {useEffect, useState } from 'react';
-import axios from "axios"
+import axios from 'axios'
 import '../App.css'
+import NumberFormat from 'react-number-format'
 
 
 const Global = () => {
-  const [confirmed, setConfirmed] = useState([]);
-  const [deaths, setDeaths] = useState([]);
-  const [recovered, setRecovered] = useState([]);
+  const [confirmed, setConfirmed] = useState("");
+  const [deaths, setDeaths] = useState("");
+  const [recovered, setRecovered] = useState("");
   useEffect(() => {
     axios 
      .get("https://covid19.mathdro.id/api")
@@ -25,9 +26,9 @@ const Global = () => {
     <h3><center>Jumlah kasus di Seluruh Dunia</center></h3>
     <center><img src="/images/global.jpg" alt=""/></center>
     <div className = "box">
-    <h1 className ="boxConfirmed">Positif {confirmed}</h1>
-    <h1 className ="boxDeaths">Meninggal {deaths}</h1>
-    <h1 className ="boxRecovered">Sembuh {recovered}</h1>
+    <h1 className ="boxConfirmed">Positif <NumberFormat value={confirmed} displayType={'text'} thousandSeparator={true} /></h1>
+    <h1 className ="boxDeaths">Meninggal <NumberFormat value={deaths} displayType={'text'} thousandSeparator={true} /></h1>
+    <h1 className ="boxRecovered">Sembuh <NumberFormat value={recovered} displayType={'text'} thousandSeparator={true} /></h1>
     </div>
     </div>
     
--- covidtracker/src/pages/Indonesia.js
+++ covidtracker/src/pages/Indonesia.js
@@ -1,12 +1,12 @@
 import React, {useEffect, useState } from 'react';
-import axios from "axios"
+import axios from 'axios'
 import '../App.css'
 import NumberFormat from 'react-number-format'
 
 const Indonesia = () => {
-  const [jumlahKasus, setjumlahKasus] = useState([]);
-  const [meninggal, setmeninggal] = useState([]);
-  const [sembuh, setsembuh] = useState([]);
+  const [jumlahKasus, setjumlahKasus] = useState("");
+  const [meninggal, setmeninggal] = useState("");
+  const [sembuh, setsembuh] = useState("");
   useEffect(() => {
     axios 
      .get("https://indonesia-covid-19.mathdro.id/api")
@@ -26,9 +26,9 @@ const Indonesia = () => {
     <h3><center>Jumlah kasus di Indonesia</center></h3>
     <center><img src="/images/indo.jpg" alt=""/></center>
     <div className = "box">
-    <h1 className ="boxConfirmed">Positif {jumlahKasus}</h1>
-    <h1 className ="boxDeaths">Meninggal {meninggal}</h1>
-    <h1 className ="boxRecovered">Sembuh {sembuh}</h1>
+    <h1 className ="boxConfirmed">Positif <NumberFormat value={jumlahKasus} displayType={'text'} thousandSeparator={true} /></h1>
+    <h1 className ="boxDeaths">Meninggal <NumberFormat value={meninggal} displayType={'text'} thousandSeparator={true} /></h1>
+    <h1 className ="boxRecovered">Sembuh <NumberFormat value={sembuh} displayType={'text'} thousandSeparator={true} /></h1>
     </div>
     </div>
     
